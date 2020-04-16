cee9e272de8a669868a0693e110422d7856a8c74
Talita Azevedo <talita.azevedo360@gmail.com>
2020-04-16 20:38:27 -0300

    upload de avatar

--- src/models/User.ts
+++ src/models/User.ts
@@ -21,6 +21,9 @@ class User {
   email: string;
 
   @Column()
+  avatar: string;
+
+  @Column()
   password: string;
 
   @CreateDateColumn()
--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -4,6 +4,8 @@ import multer from 'multer';
 import uploadConfig from '../config/upload';
 
 import CreateUserService from '../services/CreateUserService';
+import UpdateUserAvatarService from '../services/UpdateUserAvatarService';
+
 import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 
 const usersRouter = Router();
@@ -39,12 +41,28 @@ usersRouter.post('/', async (request, response) => {
 });
 
 usersRouter.patch('/avatar', ensureAuthenticated, upload.single('avatar'), async (request, response) => {
+  try {
+    const updateUserAvatar = new UpdateUserAvatarService();
 
+    const user = await updateUserAvatar.execute({
+      user_id: request.user.id,
+      avatarFilename: request.file.filename,
+    });
+
+    delete user.password;
+    return response.json(user);
+
+
+  } catch (err) {
+
+    return response.status(400).json({ error: err.message });
+
+  }
   console.log(request.file);
 
 
 
-  return response.json({ ok: true });
+
 });
 
 
