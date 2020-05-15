8bad5fa117b74a01a074479ed1c9cbaeae27da7a
Talita Azevedo <talita.azevedo360@gmail.com>
2020-05-15 17:39:52 -0300

    Pensando nos Testes

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,3 +1,12 @@
-test('sum two numbers', () => {
-  expect(1 + 2).toBe(3);
-});
+import CreateAppointmentService from '@modules/appointments/services/CreateAppointmentService.spec';
+
+describe('CreateAppointment', () => {
+  it('should be able to create a new appointment', () => {
+    expect(1 + 2).toBe(3)
+  });
+
+
+//   it('should not be able to create two appointments on the same time', () => {});
+// });
+
+/* ? Existem duas formas de  criar os testes, utilizando a função it() ou a função teste() */
