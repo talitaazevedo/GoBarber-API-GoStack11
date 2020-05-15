ac278cc1454cee68720e4d383298564d3ff4a906
Talita Azevedo <talita.azevedo360@gmail.com>
2020-05-15 18:26:55 -0300

    Criando Primeiro Teste

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,10 +1,21 @@
-import CreateAppointmentService from '@modules/appointments/services/CreateAppointmentService.spec';
+import FakeAppointmentsRepository from '@modules/appointments/repositories/fakes/FakeAppointmentsRepository';
+import CreateAppointmentService from '@modules/appointments/services/CreateAppointmentService';
 
 describe('CreateAppointment', () => {
-  it('should be able to create a new appointment', () => {
-    expect(1 + 2).toBe(3)
-  });
+  it('should be able to create a new appointment', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    const createAppointment = new CreateAppointmentService(
+      fakeAppointmentsRepository,
+    );
+    const appointment = await createAppointment.execute({
+      date: new Date(),
+      provider_id: '123',
+    });
 
+    expect(appointment).toHaveProperty('id');
+    expect(appointment.provider_id).toBe('123');
+  });
+});
 
 //   it('should not be able to create two appointments on the same time', () => {});
 // });
