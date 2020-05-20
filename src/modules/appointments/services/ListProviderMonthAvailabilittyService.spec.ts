1c05cba818389f7642cb851e5922e74d42067e38
Talita Azevedo <talita.azevedo360@gmail.com>
2020-05-20 13:35:13 -0300

    Listando dias disponíveis

--- src/modules/appointments/services/ListProviderMonthAvailabilittyService.spec.ts
+++ src/modules/appointments/services/ListProviderMonthAvailabilittyService.spec.ts
@@ -16,19 +16,56 @@ describe('ListProviderMonthAvailability', () => {
   it('should be able to list  the month availability from provider', async () => {
     await fakeAppointmentsRepository.create({
       provider_id: 'user',
-      date: new Date(2020, 3, 20, 8, 0, 0),
-    });
-    await fakeAppointmentsRepository.create({
-      provider_id: 'user',
-      date: new Date(2020, 4, 20, 8, 0, 0),
-    });
-    await fakeAppointmentsRepository.create({
-      provider_id: 'user',
-      date: new Date(2020, 4, 20, 10, 0, 0),
-    });
-    await fakeAppointmentsRepository.create({
-      provider_id: 'user',
       date: new Date(2020, 4, 21, 8, 0, 0),
+    });
+    await fakeAppointmentsRepository.create({
+      provider_id: 'user',
+      date: new Date(2020, 4, 21, 9, 0, 0),
+    });
+    await fakeAppointmentsRepository.create({
+      provider_id: 'user',
+      date: new Date(2020, 4, 21, 10, 0, 0),
+    });
+    await fakeAppointmentsRepository.create({
+      provider_id: 'user',
+      date: new Date(2020, 4, 21, 11, 0, 0),
+    });
+    await fakeAppointmentsRepository.create({
+      provider_id: 'user',
+      date: new Date(2020, 4, 21, 12, 0, 0),
+    });
+    await fakeAppointmentsRepository.create({
+      provider_id: 'user',
+      date: new Date(2020, 4, 21, 13, 0, 0),
+    });
+    await fakeAppointmentsRepository.create({
+      provider_id: 'user',
+      date: new Date(2020, 4, 21, 14, 0, 0),
+    });
+    await fakeAppointmentsRepository.create({
+      provider_id: 'user',
+      date: new Date(2020, 4, 21, 14, 0, 0),
+    });
+    await fakeAppointmentsRepository.create({
+      provider_id: 'user',
+      date: new Date(2020, 4, 21, 15, 0, 0),
+    });
+    await fakeAppointmentsRepository.create({
+      provider_id: 'user',
+      date: new Date(2020, 4, 21, 16, 0, 0),
+    });
+    await fakeAppointmentsRepository.create({
+      provider_id: 'user',
+      date: new Date(2020, 4, 21, 17, 0, 0),
+    });
+    await fakeAppointmentsRepository.create({
+      provider_id: 'user',
+      date: new Date(2020, 4, 21, 18, 0, 0),
+    });
+
+    await fakeAppointmentsRepository.create({
+      provider_id: 'user',
+      date: new Date(2020, 4, 22, 8, 0, 0),
     });
 
     const availability = await listProviderMonthAvailability.execute({
@@ -41,8 +78,6 @@ describe('ListProviderMonthAvailability', () => {
     // espero que availability seja igual e espero um array contendo dados que vou informar
     expect(availability).toEqual(
       expect.arrayContaining([
-        { day: 19, available: true },
-        { day: 20, available: false },
         { day: 21, available: false },
         { day: 22, available: true },
       ]),
--- src/modules/appointments/services/ListProviderMonthAvailabilittyService.ts
+++ src/modules/appointments/services/ListProviderMonthAvailabilittyService.ts
@@ -2,6 +2,7 @@
 
 import 'reflect-metadata';
 import { injectable, inject } from 'tsyringe';
+import { getDaysInMonth, getDate } from 'date-fns';
 
 import AppError from '@shared/errors/AppError';
 import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
@@ -30,6 +31,7 @@ class ListProviderMonthAvailabilittyService {
     year,
     month,
   }: IRequest): Promise<IResponse> {
+    // Fiz uma unica query do Banco
     const appointments = await this.appointmentsRepository.findAllInMonthFromProvider(
       {
         provider_id,
@@ -37,8 +39,32 @@ class ListProviderMonthAvailabilittyService {
         month,
       },
     );
-    console.log(appointments);
-    return [{ day: 1, available: false }];
+    // os meses no javascript nativo começam com 0 janeiro e vai até 11 dezembro por isso estou tirando um número da variável month
+
+    const numberOfDaysInMonth = getDaysInMonth(new Date(year, month - 1));
+    // Array.from => cria um array a partir de alguma coisa.
+    // Formar um array com os dias d mes
+    const eachDayArray = Array.from(
+      { length: numberOfDaysInMonth },
+      // isso aqui faz o index do array começar em 1
+      (_, index) => index + 1,
+    );
+
+    // Aqui eu faço apenas um filtro para buscar  as
+    //  informações da query
+    const availability = eachDayArray.map(day => {
+      // faz um filtro e essa função do datefns getDAte retorna um dia
+      const appointmentsInDay = appointments.filter(appointment => {
+        return getDate(appointment.date) === day;
+      });
+      // Retorna um objeto, available retorna true ou false,
+      // se for menor que 10 quer dizer que eu tenho  ao menos um horário livre no dia, ai ele retorna true.
+      return { day, available: appointmentsInDay.length < 10 };
+    });
+
+    console.log('Availability', availability);
+
+    return availability;
   }
 }
 
