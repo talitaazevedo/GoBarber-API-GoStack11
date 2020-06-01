070e16a6f57affaa0c210c5067cf4ef58319d014
Talita Azevedo <talita.azevedo360@gmail.com>
2020-06-01 18:34:32 -0300

    Organizando o Container

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -1,29 +1,4 @@
-import { container } from 'tsyringe';
-import mailConfig from '@config/mail';
-
-import IStorageProvider from '../providers/StorageProviders/models/IStorageProvider';
-import DiskStorageProvider from './StorageProviders/implementations/DiskStorageProvider';
-
-import EtherealMailProvider from './MailProvider/implementations/EtherealMailProvider';
-import SESMailProvider from './MailProvider/implementations/SESMailProvider';
-
-import IMailProvider from './MailProvider/models/IMailProvider';
-import IMailTemplateProvider from './MailTemplateProvider/models/IMailTemplateProvider';
-
-import HandlebarsMailTemplateProvider from './MailTemplateProvider/implementations/HandlkebarsMailTemplateProvider';
-
-container.registerSingleton<IStorageProvider>(
-  'StorageProvider',
-  DiskStorageProvider,
-);
-container.registerSingleton<IMailTemplateProvider>(
-  'MailTemplateProvider',
-  HandlebarsMailTemplateProvider,
-);
-// Criando a injeção de Dependencia
-container.registerInstance<IMailProvider>(
-  'MailProvider',
-  mailConfig.driver === 'ethereal'
-    ? container.resolve(EtherealMailProvider)
-    : container.resolve(SESMailProvider),
-);
+// Import de configurations de cada providers
+import './MailTemplateProvider';
+import './MailProvider';
+import './StorageProviders';
