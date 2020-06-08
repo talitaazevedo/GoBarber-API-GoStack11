eef7f44450a154dfe2f05e68ec2d9432c2730232
Talita Azevedo <talita.azevedo360@gmail.com>
2020-06-08 14:47:42 -0300

    Utilizando Query Params

--- src/modules/appointments/infra/http/controllers/ProviderDayAvailabilityController copy.ts
+++ src/modules/appointments/infra/http/controllers/ProviderDayAvailabilityController copy.ts
@@ -6,15 +6,18 @@ import ListProviderDayAvailabilityService from '@modules/appointments/services/L
 export default class ProviderDayAvailabilityController {
   public async index(request: Request, response: Response): Promise<Response> {
     const { provider_id } = request.params;
-    const { month, year, day } = request.body;
+    const { month, year, day } = request.query;
+    // Requisições do tipo GET => não podem conter corpo da requisição
+    // Os query params  recebem as variáveis no formato string, e como estamos trabalhando com numeros será necessário converter.
+
     // Importamos o container do tsyringe e passamos resolve() instanciar  o repositorio.
     const listProviders = container.resolve(ListProviderDayAvailabilityService);
 
     const availability = await listProviders.execute({
       provider_id,
-      year,
-      month,
-      day,
+      year: Number(year),
+      month: Number(month),
+      day: Number(day),
     });
 
     return response.json(availability);
--- src/modules/appointments/infra/http/controllers/ProviderMonthAvailabilityController.ts
+++ src/modules/appointments/infra/http/controllers/ProviderMonthAvailabilityController.ts
@@ -5,7 +5,7 @@ import ListProviderMonthAvailabilityService from '@modules/appointments/services
 export default class ProviderMonthAvailabilityController {
   public async index(request: Request, response: Response): Promise<Response> {
     const { provider_id } = request.params;
-    const { month, year } = request.body;
+    const { month, year } = request.query;
     // Importamos o container do tsyringe e passamos resolve() instanciar  o repositorio.
     const listProviders = container.resolve(
       ListProviderMonthAvailabilityService,
@@ -13,8 +13,8 @@ export default class ProviderMonthAvailabilityController {
 
     const availability = await listProviders.execute({
       provider_id,
-      month,
-      year,
+      month: Number(month),
+      year: Number(year),
     });
 
     return response.json(availability);
