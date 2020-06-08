18455dde884bd9485e9dae5ebf546d62832d9d94
Talita Azevedo <talita.azevedo360@gmail.com>
2020-06-08 16:39:32 -0300

    Serialização no cache

--- src/modules/appointments/services/ListProviderAppointmentService.ts
+++ src/modules/appointments/services/ListProviderAppointmentService.ts
@@ -7,6 +7,7 @@ import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
 import Appointment from '../infra/typeorm/entities/Appointment';
 
 import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
+import { classToClass } from 'class-transformer';
 
 interface IRequest {
   provider_id: string;
@@ -45,7 +46,7 @@ class ListProviderAppointmentService {
         },
       );
       console.log('buscou do banco');
-      await this.cacheProvider.save(cacheKey, appointments);
+      await this.cacheProvider.save(cacheKey, classToClass(appointments));
     }
 
     return appointments;
